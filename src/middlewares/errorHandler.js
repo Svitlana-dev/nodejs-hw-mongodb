e32409d1aef8b690e51c3f2f9a8beff20b815911
Svitlana Potiichuk <ladys1813@gmail.com>
2025-07-13 16:28:10 +0300

    Fix: return 404 when contact not found

--- src/middlewares/errorHandler.js
+++ src/middlewares/errorHandler.js
@@ -1,8 +1,9 @@
 // eslint-disable-next-line no-unused-vars
 export default function errorHandler(err, req, res, _next) {
-  res.status(500).json({
-    status: 500,
-    message: 'Something went wrong',
+  const status = err.status || err.statusCode || 500;
+  res.status(status).json({
+    status,
+    message: err.message || 'Something went wrong',
     data: err.message,
   });
 }
